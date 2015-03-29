6f875e2110f9a1f6d212634b32ac8ae9146efbfa
fotcorn <fotcorn@gmail.com>
2015-03-29 20:15:30 +0200

    set1 challenge3: skip strings with unprintable characters

--- src/bin/set1_challenge3.rs
+++ src/bin/set1_challenge3.rs
@@ -1,7 +1,7 @@
 use std::str::{from_utf8};
 
 extern crate rustc_serialize;
-use rustc_serialize::hex::{FromHex, ToHex};
+use rustc_serialize::hex::{FromHex};
 
 fn main() {
 	let a = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".from_hex().unwrap();
@@ -12,15 +12,22 @@ fn main() {
 		vec.push(0);
 	}
 
-	for char in 0..255 {
+	'char_loop: for char in 0..255 {
 		for i in 0..a.len() {
-			vec[i] = a[i] ^ char;
+			match a[i] ^ char {
+				0x20...0x7E => {
+					vec[i] = a[i] ^ char;
+				}
+				_ => {
+					continue 'char_loop;
+				}
+			}
 		}
 		match from_utf8(&vec) {
 			Ok(v) => {
 				println!("{}", v);
 			}
-            Err(e) => {}
+            _ => {}
 		}
 	}
 }
