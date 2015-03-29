47da50a7aa942a0910051a0ddc73b63492f284e0
fotcorn <fotcorn@gmail.com>
2015-03-29 23:19:45 +0200

    set1 challenge5: added

---
+++ src/bin/set1_challenge5.rs
@@ -0,0 +1,15 @@
+extern crate rustc_serialize;
+use rustc_serialize::hex::ToHex;
+
+fn main() {
+	let string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".as_bytes();
+	let key = "ICE".as_bytes();
+
+	let mut encrypted: Vec<u8> = Vec::new();
+
+	for i in 0..string.len() {
+		encrypted.push(string[i] ^ key[i % 3])
+	}
+
+	println!("{}", encrypted.to_hex());
+}
