4556dd22f33849ba996445c063f1fab51671eb7d
fotcorn <fotcorn@gmail.com>
2015-03-29 21:16:04 +0200

    set1 challenge3: analyse character count to find correct string

--- src/bin/set1_challenge3.rs
+++ src/bin/set1_challenge3.rs
@@ -1,4 +1,4 @@
-use std::str::{from_utf8};
+use std::str;
 
 extern crate rustc_serialize;
 use rustc_serialize::hex::{FromHex};
@@ -12,22 +12,58 @@ fn main() {
 		vec.push(0);
 	}
 
+	let mut max_score: u64 = 0;
+	let mut best_string: Vec<u8> = Vec::new();
+
 	'char_loop: for char in 0..255 {
+		let mut score: u64 = 0;
 		for i in 0..a.len() {
 			match a[i] ^ char {
 				0x20...0x7E => {
 					vec[i] = a[i] ^ char;
+					score += get_score(vec[i] as char);
 				}
 				_ => {
 					continue 'char_loop;
 				}
 			}
 		}
-		match from_utf8(&vec) {
-			Ok(v) => {
-				println!("{}", v);
-			}
-            _ => {}
+		if score > max_score {
+			max_score = score;
+			best_string = vec.clone();
 		}
+	}
+	println!("{}: {}", max_score, str::from_utf8(&best_string).unwrap());
+}
+
+fn get_score(character: char) -> u64 {
+	return match character.to_uppercase().next().unwrap() {
+		'E' => 4452,
+		'T' => 3305,
+		'A' => 2865,
+		'O' => 2723,
+		'I' => 2697,
+		'N' => 2578,
+		'S' => 2321,
+		'R' => 2238,
+		'H' => 1801,
+		'L' => 1450,
+		'D' => 1360,
+		'C' => 1192,
+		'U' => 973,
+		'M' => 895,
+		'F' => 856,
+		'P' => 761,
+		'G' => 666,
+		'W' => 597,
+		'Y' => 593,
+		'B' => 529,
+		'V' => 375,
+		'K' => 193,
+		'X' => 84,
+		'J' => 57,
+		'Q' => 43,
+		'Z' => 32,
+		_ => 0,
 	}
 }
