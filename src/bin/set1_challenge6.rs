7c05256d1a0a116ac9c61201cdbf81308669f618
fotcorn <fotcorn@gmail.com>
2015-04-01 09:50:30 +0200

    set1 challenge6: added hamming distance function

--- src/bin/set1_challenge6.rs
+++ src/bin/set1_challenge6.rs
@@ -11,7 +11,8 @@ fn main() {
 		Ok(data) => data,
 	};
 
-	let mut smallest_keysize_distance = std::usize::MAX; // maxint?
+	let mut smallest_keysize_distance = std::usize::MAX;
+	let mut smallest_keysize = 0;
 
 	for key_size in 2..41 {
 		let str1 = &data[0..key_size]; // slice?
@@ -21,19 +22,21 @@ fn main() {
 
 		let mut distances = Vec::new();
 
-		distances.push(edit_distance(str1, str2) / key_size);
-		distances.push(edit_distance(str1, str3) / key_size);
-		distances.push(edit_distance(str1, str4) / key_size);
-		distances.push(edit_distance(str2, str3) / key_size);
-		distances.push(edit_distance(str2, str4) / key_size);
-		distances.push(edit_distance(str3, str4) / key_size);
+		distances.push(hamming_distance(str1, str2) / key_size);
+		distances.push(hamming_distance(str1, str3) / key_size);
+		distances.push(hamming_distance(str1, str4) / key_size);
+		distances.push(hamming_distance(str2, str3) / key_size);
+		distances.push(hamming_distance(str2, str4) / key_size);
+		distances.push(hamming_distance(str3, str4) / key_size);
 
 		let average_distance = distances.iter().fold(0, |total, &value| total + value);
 
 		if average_distance < smallest_keysize_distance {
 			smallest_keysize_distance = average_distance;
+			smallest_keysize = key_size;
 		}
 	}
+	println!("smallest_keysize_distance: {}, key_size: {}", smallest_keysize_distance, smallest_keysize);
 }
 
 
@@ -53,12 +56,24 @@ fn load_from_file() -> std::io::Result<Vec<u8>> {
 
 
 /*
+binary hamming distance
 this is a test
 wokka wokka!!!
 -> 37
  */
+fn hamming_distance(str1: &[u8], str2: &[u8]) -> usize {
+    let mut distance = 0;
 
-fn edit_distance(str1: &[u8], str2: &[u8]) -> usize {
-	return 0;
+    for i in 0..str1.len() {
+		let diff = str1[i] ^ str2[i];
+        if (diff & 0b00000001) > 0 { distance += 1; }
+        if (diff & 0b00000010) > 0 { distance += 1; }
+        if (diff & 0b00000100) > 0 { distance += 1; }
+        if (diff & 0b00001000) > 0 { distance += 1; }
+        if (diff & 0b00010000) > 0 { distance += 1; }
+        if (diff & 0b00100000) > 0 { distance += 1; }
+        if (diff & 0b01000000) > 0 { distance += 1; }
+        if (diff & 0b10000000) > 0 { distance += 1; }
+    }
+    return distance;
 }
-
