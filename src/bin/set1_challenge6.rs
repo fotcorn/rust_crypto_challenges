d0b1d0fe769ea61370ff6e4e221807174b4137d0
fotcorn <fotcorn@gmail.com>
2015-04-02 10:03:14 +0200

    set1 challenge6: refactored key size guessing in its own function

--- src/bin/set1_challenge6.rs
+++ src/bin/set1_challenge6.rs
@@ -11,34 +11,9 @@ fn main() {
 		Ok(data) => data,
 	};
 
-	let mut smallest_keysize_distance = std::usize::MAX;
-	let mut smallest_keysize = 0;
-
-	for key_size in 2..41 {
-		let str1 = &data[0..key_size]; // slice?
-		let str2 = &data[key_size..key_size*2];
-		let str3 = &data[key_size*2..key_size*3];
-		let str4 = &data[key_size*3..key_size*4];
-
-		let mut distances = Vec::new();
-
-		distances.push(hamming_distance(str1, str2) / key_size);
-		distances.push(hamming_distance(str1, str3) / key_size);
-		distances.push(hamming_distance(str1, str4) / key_size);
-		distances.push(hamming_distance(str2, str3) / key_size);
-		distances.push(hamming_distance(str2, str4) / key_size);
-		distances.push(hamming_distance(str3, str4) / key_size);
-
-		let average_distance = distances.iter().fold(0, |total, &value| total + value);
-
-		if average_distance < smallest_keysize_distance {
-			smallest_keysize_distance = average_distance;
-			smallest_keysize = key_size;
-		}
-	}
-	println!("smallest_keysize_distance: {}, key_size: {}", smallest_keysize_distance, smallest_keysize);
+	let key_size = guess_key_size(&data);
+	println!("guesses key size: {}", key_size);
 }
-
 
 fn load_from_file() -> std::io::Result<Vec<u8>> {
 	let file = try!(File::open("data/set1_challenge6.txt"));
@@ -54,6 +29,34 @@ fn load_from_file() -> std::io::Result<Vec<u8>> {
 	return Ok(decoded_data);
 }
 
+fn guess_key_size(data: &[u8]) -> usize {
+	let mut smallest_key_size_distance = std::usize::MAX;
+	let mut guessed_key_size = 0;
+
+	for key_size in 2..41 {
+		let str1 = &data[0..key_size]; // slice?
+		let str2 = &data[key_size..key_size*2];
+		let str3 = &data[key_size*2..key_size*3];
+		let str4 = &data[key_size*3..key_size*4];
+
+		let mut distances = Vec::new();
+
+		distances.push(hamming_distance(str1, str2) / key_size)                                                                          ;
+		distances.push(hamming_distance(str1, str3) / key_size);
+		distances.push(hamming_distance(str1, str4) / key_size);
+		distances.push(hamming_distance(str2, str3) / key_size);
+		distances.push(hamming_distance(str2, str4) / key_size);
+		distances.push(hamming_distance(str3, str4) / key_size)                                                                          ;
+
+		let average_distance = distances.iter().fold(0, |total, &value| total + value);
+
+		if average_distance < smallest_key_size_distance {
+			smallest_key_size_distance = average_distance;
+			guessed_key_size = key_size;
+		}
+	}
+	return guessed_key_size;
+}
 
 /*
 binary hamming distance
@@ -77,3 +80,7 @@ fn hamming_distance(str1: &[u8], str2: &[u8]) -> usize {
     }
     return distance;
 }
+
+
+
+
